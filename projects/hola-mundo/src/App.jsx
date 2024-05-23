e72dea3b089d7d1a204e9add20d413321a702807
MR PC <maximilianorivas01@gmail.com>
2024-05-23 12:49:38 -0300

    Creacion de Componente ListTwitterFollowCard

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -4,11 +4,106 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
+
+    const format = (userName) => '@' + userName;
+
+
+    // Supongamos que tengo un json que ya transforme a objeto que obtuve
+    // de la base de datos con todos los usuarios
+
+    const users = [
+        {
+            userName: 'maxi.rivas.ok',
+            name: 'Maxi Rivas',
+            initialIsFollowing: true
+        },
+        {
+            userName: 'maxi.ok',
+            name: 'Eze Rivas',
+            initialIsFollowing: false
+        },
+        {
+            userName: 'vxnder',
+            name: 'Vanderhard',
+            initialIsFollowing: true
+        },
+        {
+            userName: 'elonmusk',
+            name: 'Elon Musk',
+            initialIsFollowing: false
+        }
+    ]
+
+
+
     return (
+
+        // Notas Curso
+        // Mala practica dar estilo con margin o con padding porque podemos querer o no
+        // que este componente tenga margin en otro lado donde se lo utilice
+        // Mala practica en estados:
+        //  Los estados de un padre no afectan a los estados de un hijo
+        //  Si defino un estado aqui y a esa variable la paso en un prompt de
+        //  otro elemento hijo el hijo no tomará el cambio cada vez que la variable
+        //  cambie de estado
+        // EL ESTADO SOLO SE INICIALIZA UNA VEZ
+
         // <> == <React.Fragment>
-        <>
-            <TwitterFollowCard userName="maxi.rivas.ok" name="maxi" />
-            <TwitterFollowCard userName="maxi.ok" name="exe" />
-        </>
+        // <div className='App'>
+        //     {/* isFollowing == isFollowing = {true} */}
+        //     {/* identificadorFuncion != identificadoFuncion() uno es definicion y el otro resultado de la ejecucion */}
+        //     <TwitterFollowCard
+        //         formatUserName={format}
+        //         initialFollowing={false}
+        //         userName="maxi.rivas.ok"
+        //         name="Maxi Rivas">
+        //         {/* Con el prompt children podemos inyectar el codigo directamente en el componente */}
+        //         Soy children
+        //     </TwitterFollowCard>
+
+        //     <TwitterFollowCard
+        //         formatUserName={format}
+        //         initialFollowing={true}
+        //         userName="maxi.ok"
+        //         name="Eze Rivas">
+        //         Eze Rivas
+        //     </TwitterFollowCard>
+
+        //     <TwitterFollowCard
+        //         formatUserName={format}
+        //         initialFollowing
+        //         userName="vxnder"
+        //         name="Vanderhard">
+        //         Vanderhard
+        //     </TwitterFollowCard>
+
+        //     <TwitterFollowCard
+        //         formatUserName={format}
+        //         userName="elonmusk"
+        //         name="Elon Musk">
+        //         Elon Musk
+        //     </TwitterFollowCard>
+
+        //     <TwitterFollowCard formatUserName={format}> </TwitterFollowCard>
+
+        // </div>
+
+
+        <section className='App'>
+            {
+                users.map(({ userName, name, initialIsFollowing }) => (
+                    <TwitterFollowCard
+                        // La clave y su valor key son muy importantes porque lo necesita el DOM virtual
+                        // Es recomendable utilizar el ID que proporciona la base de datos en este caso username
+                        key={userName}
+                        userName={userName}
+                        formatUserName={format}
+                        initialIsFollowing={initialIsFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
+        </section>
     )
 }
--- projects/hola-mundo/src/TwitterFollowCard.jsx
+++ projects/hola-mundo/src/TwitterFollowCard.jsx
@@ -1,19 +1,51 @@
-
+import { useState } from "react"
 // Tener cuidado al pasar los parametros al componente {}
-export function TwitterFollowCard({ userName, name }) {
+// Al no recibir algun prompt lo recomendable es definir un valor por defecto identificadorPrompt = 'unknown'
+export function TwitterFollowCard({ children, formatUserName, userName = 'unknoWn', initialFollowing }) {
+
+    // mala practica: las prompts del componente no pueden ser mutables si
+    // username = "123"+username   MAL
+    // cont userNameMod = "123"+username   BIEN
+
+    // Definimos estados en react de manera que se tome el estado que se definio en App.jsx
+    const [isFollowing, setIsFollowing] = useState(initialFollowing)
+    //  ==
+    // const state = useState(false)
+    // const isFollowing = state[0];
+    // const setisFollowing = state[1];
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ?
+        'tw-followCard-button is-following' :
+        'tw-followCard-button'
+
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 {/* caso particular de los enlaces dinamicos (src) */}
-                <img className='tw-followCard-avatar' src={`https://unavatar.io/${name}`} alt="Avatar de Maxi" />
+                <img
+                    className='tw-followCard-avatar'
+                    src={`https://unavatar.io/${userName}`}
+                    alt={`El avatar de ${userName}`} />
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>
+                        {formatUserName(userName)}
+                    </span>
                 </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className="tw-followCard-text">
+                        {text}
+                    </span>
+                    <span className="tw-followCard-stopFollow">
+                        Dejar de Seguir
+                    </span>
                 </button>
             </aside>
         </article>
